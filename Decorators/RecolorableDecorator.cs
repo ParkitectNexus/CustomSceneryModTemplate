67107a4efc67379c82e960261229f008497620c7
Luuk Holleman <luukholleman@gmail.com>
2015-11-01 12:30:12 +0100

    Added 'recolorable' option

---
+++ Decorators/RecolorableDecorator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Custom_Scenery.Decorators
+{
+    class RecolorableDecorator : IDecorator
+    {
+        private bool _recolorable;
+
+        public RecolorableDecorator(bool recolorable)
+        {
+            _recolorable = recolorable;
+        }
+
+        public void Decorate(GameObject go, Dictionary<string, object> options, AssetBundle assetBundle)
+        {
+            if (go.GetComponent<BuildableObject>() != null && _recolorable)
+            {
+                go.AddComponent<CustomColors>();
+            }
+        }
+    }
+}
